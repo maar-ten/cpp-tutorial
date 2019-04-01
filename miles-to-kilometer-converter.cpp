5de42d7b359d24a376c4f061423c12c06caefa10
maar-ten <maar-ten@users.noreply.github.com>
2019-04-01 10:42:15 +0200

    Added exercise 1 and 2

---
+++ miles-to-kilometer-converter.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+
+int no_main() {
+    std::string sQuestion("How many miles?\n");
+    std::string sMiles;
+
+    std::cout << sQuestion;
+    getline(std::cin, sMiles);
+
+    double dMiles = std::stod(sMiles);
+    double dKilometers = dMiles * 1.60394;
+
+    printf("%.1f miles is equal to %.4f3 kilometers", dMiles, dKilometers);
+}
