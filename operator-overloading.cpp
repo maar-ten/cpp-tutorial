dd149e4647dcd6e3f9791487d6a5c12fbec6418d
maar-ten <maar-ten@users.noreply.github.com>
2019-04-02 22:09:03 +0200

    Added operator overloading example

--- operator-overloading.cpp
+++ operator-overloading.cpp
@@ -1,4 +1,27 @@
-//
-// Created by MLite on 02/04/2019.
-//
+#include <iostream>
 
+class Box {
+public:
+    double length;
+    double height;
+    double breadth;
+
+    Box(double length, double height, double breadth) : length(length), height(height), breadth(breadth) {}
+
+    void operator +(Box &box) {
+        length = box.length;
+        height = box.height;
+        breadth = box.breadth;
+    };
+};
+
+int main() {
+    Box box(1, 1, 1);
+    Box box2(10, 10, 10);
+
+    box + box2;
+
+    std::cout << box.length << "\n";
+
+    return 0;
+}
