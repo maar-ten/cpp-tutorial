6fc3123aad0fd3e0a2917b9948bfa533667b1f8d
maar-ten <maar-ten@users.noreply.github.com>
2019-04-03 22:41:23 +0200

    Added answer to heads and tails question

--- heads-and-tails.cpp
+++ heads-and-tails.cpp
@@ -1,4 +1,28 @@
-//
-// Created by MLite on 03/04/2019.
-//
+#include <ctime>
+#include <cstdlib>
+#include <iostream>
 
+void generateHeadsAndTails(int numberOfThrows, int &heads, int &tails) {
+    for (int i = 0; i < numberOfThrows; i++) {
+        int thrown = std::rand() % 2;
+        if (thrown == 0) {
+            heads++;
+        } else {
+            tails++;
+        }
+    }
+}
+
+int main() {
+    srand(time(nullptr));
+
+    int heads = 0;
+    int tails = 0;
+
+    generateHeadsAndTails(100, heads, tails);
+
+    std::cout << "Heads : " << heads << "\n";
+    std::cout << "Tails : " << tails << "\n";
+
+    return 0;
+}
